21d56b54ccc8b8e4d45c5f1459d9235fffd2ece0
Jairo Segura <jairoasb@gmail.com>
2024-01-19 16:05:36 +0100

    progress task02

--- task02/src/main.cpp
+++ task02/src/main.cpp
@@ -1,6 +1,44 @@
-// Hello World
+// Guess random number
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+int number (){
+  int n;
+  srand(time(NULL));
+  n = rand() % 5;
+  return n;
+}
+
+int check(int a, int b){
+  if (a == b){
+    std::cout << "correct!";
+    return 0;
+  }
+  else{
+    std::cout << "incorrect!";
+    return 1;
+  }
+}
+
 int main (){
+  int i;
+  int guess;
+  int n=number();
+
+  for (i=0;i<3;i++){
   std::cout << "enter number between 0 and 5" <<std::endl;
+  std::cin >> guess;
+
+  if(std::cin.fail()){
+    std::cout << "that was not a number" <<std::endl;
+    std::cin.clear();
+  return 0;
+  }
+  else {
+    std::cout << "using " << guess << " as your guess" << std::endl;}
+  check(guess,n);
+  }
+  std::cout << "the number was " << n << std::endl;
   return 0;
 }
