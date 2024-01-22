26f73424f3c6cfb785a3cd7a341076ca33277095
Jairo Segura <jairoasb@gmail.com>
2024-01-22 17:13:57 +0100

    add clang

--- task02/src/main.cpp
+++ task02/src/main.cpp
@@ -27,17 +27,17 @@ int main (){
   int n=number();
 
   for (i=0;i<3;i++){
-  std::cout << "enter number between 0 and 5" <<std::endl;
-  std::cin >> guess;
+    std::cout << "enter number between 0 and 5" <<std::endl;
+    std::cin >> guess;
 
-  if(std::cin.fail()){
-    std::cout << "that was not a number" <<std::endl;
-    std::cin.clear();
-  return 0;
-  }
-  else {
-    std::cout << "using " << guess << " as your guess" << std::endl;}
-  check(guess,n);
+    if(std::cin.fail()){
+      std::cout << "that was not a number" <<std::endl;
+      std::cin.clear();
+      return 0;
+    }
+    else {
+      std::cout << "using " << guess << " as your guess" << std::endl;}
+    check(guess,n);
   }
   std::cout << "the number was " << n << std::endl;
   return 0;
